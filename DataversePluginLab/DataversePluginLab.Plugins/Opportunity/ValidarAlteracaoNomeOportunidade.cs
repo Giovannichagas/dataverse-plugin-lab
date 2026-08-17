5529ca79b9aa0b39bdca3dee0562c419ee43f137
Giovannichagas <giovanni0012@hotmail.com>
2026-08-17 12:06:54 +0200

    feat: compara nome da oportunidade usando PreImage

--- DataversePluginLab/DataversePluginLab.Plugins/Opportunity/ValidarAlteracaoNomeOportunidade.cs
+++ DataversePluginLab/DataversePluginLab.Plugins/Opportunity/ValidarAlteracaoNomeOportunidade.cs
@@ -9,9 +9,37 @@ namespace DataversePluginLab.Plugins.Opportunity
 {
     public class ValidarAlteracaoNomeOportunidade : PluginBase
     {
-        protected override  void ExecutePlugin()
+        protected override void ExecutePlugin()
         {
+            Entity target = ObterTarget();
 
+            if (target == null)
+            {
+                return;
+            }
+            string nomeOportunidade = target.GetAttributeValue<string>("gbc_name");
+
+            if (!Context.PreEntityImages.Contains("PreImage"))
+            {
+                return;
+            }
+
+            Entity preImage = Context.PreEntityImages["PreImage"];
+            string nomeAnterior = preImage.GetAttributeValue<string>("gbc_name");
+
+            if(nomeAnterior != nomeOportunidade)
+            {
+                TracingService.Trace("Nome da oportunidade alterado de '{0}' para '{1}' ", nomeAnterior, nomeOportunidade);
+            }
+
+        }
+        private Entity ObterTarget()
+        {
+            if (!Context.InputParameters.Contains("Target") || !(Context.InputParameters["Target"] is Entity target))
+            {
+                return null;
+            }
+            return target;
         }
     }
 }
